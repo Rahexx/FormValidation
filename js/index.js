9bbf12464c3778222d7b344fa053fec9d914e8fa
Andrzej Łuszcz <endriu1297@gmail.com>
2020-04-10 17:53:21 +0200

    add eslint and improvement code appearance

--- js/index.js
+++ js/index.js
@@ -1,51 +1,48 @@
-import {
-    validateForm
-} from './validateForm.js';
+import validateForm from './validateForm';
 
 const button = document.querySelector('button');
 
 function clearInputs(form) {
-    for (let i = 0; i < form[0].length; i++) {
-        form[0][i].value = "";
-        if (form[0][i].type == "checkbox") form[0][i].checked = false;
-    }
+  for (let i = 0; i < form[0].length; i++) {
+    form[0][i].value = '';
+    if (form[0][i].type === 'checkbox') form[0][i].checked = false;
+  }
 }
 
-button.addEventListener('click', function (e) {
-    const form = document.forms;
-    const result = validateForm(form);
-    const API_URL = 'https://przeprogramowani.pl/projekt-walidacja';
+button.addEventListener('click', (e) => {
+  const form = document.forms;
+  const result = validateForm(form);
+  const API_URL = 'https://przeprogramowani.pl/projekt-walidacja';
 
-    if (result === true) {
-        const data = {
-            name: form[0][0].value,
-            email: form[0][1].value,
-            password: form[0][2].value,
-            rodo: form[0][4].value
+  if (result === true) {
+    const data = {
+      name: form[0][0].value,
+      email: form[0][1].value,
+      password: form[0][2].value,
+      rodo: form[0][4].value,
+    };
+
+    fetch(API_URL, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.text();
         }
+        throw 'Nie udało się wysłać zapytania';
+      })
+      .then((res) => {
+        console.log(res.json());
+      })
+      .catch((err) => {
+        alert('Spróbuj ponownie');
+      });
+  }
 
-        fetch(API_URL, {
-                method: "POST",
-                mode: 'no-cors',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data),
-            })
-            .then(res => {
-                if (res.ok) {
-                    return res.text()
-                }
-                throw 'Nie udało się wysłać zapytania'
-            })
-            .then(res => {
-                console.log(res.json())
-            })
-            .catch(err => {
-                alert('Spróbuj ponownie')
-            })
-    }
-
-    clearInputs(document.forms);
-
-})
+  clearInputs(document.forms);
+});
--- js/validateForm.js
+++ js/validateForm.js
@@ -1,78 +1,89 @@
-//Comment this line when you use test
-const allHide = document.querySelectorAll(".hide");
-
-function validateForm(form) {
-    const flagName = validateName(form[0][0].value);
-    const flagEmail = validateEmail(form[0][1].value);
-    const flagPassword = validatePassword(form[0][2].value);
-    const flagConfirm = confirm(form[0][2].value, form[0][3].value);
-    const flagChecked = form[0][4].checked;
-
-    if (flagChecked === false) showError(4);
-    else hideError(4);
-
-    return flagName && flagEmail && flagPassword && flagConfirm && flagChecked ? true : false;
-}
-
-function validateName(name) {
-    const correctLength = name.length > 2 ? true : false;
-    const noNumbersAndSpecial = /^[a-zA-Z]*$/.test(name) ? true : false;
-    const validationResult = correctLength && noNumbersAndSpecial ? true : false;
-
-    if (validationResult === false) showError(0);
-    else hideError(0);
-
-    return validationResult;
-}
-
-function validateEmail(email) {
-    const haveAt = email.includes("@") ? true : false;
-    if (!haveAt) {
-        showError(1);
-        return false;
-    }
-
-    const haveText = email.split("@")[0].length > 1 ? true : false;
-    const haveDomain = email.split("@")[1].length >= 5 && email.split("@")[1].includes(".") && (email.split("@")[1].indexOf(".") > 1 || email.split("@")[1].indexOf(".") < email.split("@")[1].length - 1);
-    const validationResult = haveText && haveAt && haveDomain ? true : false
-
-    if (validationResult === false) showError(1);
-
-    return validationResult;
-}
-
-function validatePassword(password) {
-    const haveCorrectLength = password.length >= 8 ? true : false;
-    const haveNumber = /\d/.test(password);
-    const haveSpecialSymbol = !/^[a-zA-Z0-9]*$/.test(password) ? true : false;
-    const haveCapitalLetter = /[A-Z]/.test(password)
-    const validationResult = haveCorrectLength && haveNumber && haveSpecialSymbol && haveCapitalLetter ? true : false
-
-    if (validationResult === false) showError(2);
-
-    return validationResult;
-}
-
-function confirm(password, repeatPassword) {
-    const validationResult = password === repeatPassword && password != "";
-    if (validationResult === false) showError(3);
-
-    return validationResult;
-}
+// Comment this line when you use test
+const allHide = document.querySelectorAll('.hide');
 
 function showError(item) {
-    //Comment this line when you use test
-    allHide[item].classList.remove("hide");
+  // Comment this line when you use test
+  allHide[item].classList.remove('hide');
 }
 
 function hideError(item) {
-    //Comment this line when you use test
-    if (!allHide[item].classList.contains("hide")) allHide[item].classList.add("hide");
+  // Comment this line when you use test
+  if (!allHide[item].classList.contains('hide')) {
+    allHide[item].classList.add('hide');
+  }
 }
 
-export {
-    validateForm
-};
+function validateName(name) {
+  const correctLength = name.length > 2;
+  const noNumbersAndSpecial = !!/^[a-zA-Z]*$/.test(name);
+  const validationResult = !!(correctLength && noNumbersAndSpecial);
+
+  if (validationResult === false) showError(0);
+  else hideError(0);
+
+  return validationResult;
+}
+
+function validateEmail(email) {
+  const haveAt = !!email.includes('@');
+  if (!haveAt) {
+    showError(1);
+    return false;
+  }
+
+  const haveText = email.split('@')[0].length > 1;
+  const haveDomain = email.split('@')[1].length >= 5
+    && email.split('@')[1].includes('.')
+    && (email.split('@')[1].indexOf('.') > 1
+      || email.split('@')[1].indexOf('.') < email.split('@')[1].length - 1);
+  const validationResult = !!(haveText && haveAt && haveDomain);
+
+  if (validationResult === false) showError(1);
+  else hideError(1);
+
+  return validationResult;
+}
+
+function validatePassword(password) {
+  const haveCorrectLength = password.length >= 8;
+  const haveNumber = /\d/.test(password);
+  const haveSpecialSymbol = !/^[a-zA-Z0-9]*$/.test(password);
+  const haveCapitalLetter = /[A-Z]/.test(password);
+  const validationResult = !!(
+    haveCorrectLength
+    && haveNumber
+    && haveSpecialSymbol
+    && haveCapitalLetter
+  );
+
+  if (validationResult === false) showError(2);
+  else hideError(2);
+
+  return validationResult;
+}
+
+function confirm(password, repeatPassword) {
+  const validationResult = password === repeatPassword && password !== '';
+  if (validationResult === false) showError(3);
+  else hideError(3);
+
+  return validationResult;
+}
+
+function validateForm(form) {
+  const flagName = validateName(form[0][0].value);
+  const flagEmail = validateEmail(form[0][1].value);
+  const flagPassword = validatePassword(form[0][2].value);
+  const flagConfirm = confirm(form[0][2].value, form[0][3].value);
+  const flagChecked = form[0][4].checked;
+
+  if (flagChecked === false) showError(4);
+  else hideError(4);
+
+  return flagName && flagEmail && flagPassword && flagConfirm && flagChecked;
+}
+
+export default validateForm;
 
 // exports for test
 // module.exports = {
